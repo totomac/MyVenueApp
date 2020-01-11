5df12bab99bc50647ef3b55910f2ba0c3f14f7c2
Thomas <thomas.macquart@gmail.com>
2020-01-11 19:43:14 +0100

    catch network errors

--- app/src/main/java/com/thomasmacquart/apps/venueapp/core/BaseRepository.kt
+++ app/src/main/java/com/thomasmacquart/apps/venueapp/core/BaseRepository.kt
@@ -6,9 +6,13 @@ import java.io.IOException
 open class BaseRepository{
 
     suspend fun <T: Any> safeApiResult(call: suspend ()-> Response<T>) : AsyncResponse<T>{
-        val response = call.invoke()
-        if(response.isSuccessful) return AsyncResponse.Success(response.body()!!)
+        try {
+            val response = call.invoke()
+            if (response.isSuccessful) return AsyncResponse.Success(response.body()!!)
 
-        return AsyncResponse.Failed(IOException("Error Occurred during getting safe Api result"))
+            return AsyncResponse.Failed(IOException("Error Occurred during getting safe Api result"))
+        } catch (e : Exception) {
+            return AsyncResponse.Failed(IOException("Error has occured"))
+        }
     }
 }
