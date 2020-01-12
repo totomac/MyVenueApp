ea50d4204098d9cabbb044f36fa5e55b20eed0e5
Thomas <thomas.macquart@gmail.com>
2020-01-12 17:51:15 +0100

    update safe api call

---
+++ app/src/main/java/com/thomasmacquart/apps/venueapp/core/utils/NetworkUtil.kt
@@ -0,0 +1,17 @@
+package com.thomasmacquart.apps.venueapp.core.utils
+
+import com.thomasmacquart.apps.venueapp.core.AsyncResponse
+import retrofit2.Response
+import java.io.IOException
+
+suspend fun <T : Any> safeApiCall(errorMessage: String, call: suspend () -> Response<T>): AsyncResponse<T> {
+    return try {
+        val response = call.invoke()
+        if (response.isSuccessful) return AsyncResponse.Success(response.body()!!)
+
+        return AsyncResponse.Failed(IOException(errorMessage))
+    } catch (e: Exception) {
+        // An exception was thrown when calling the API so we're converting this to an IOException
+        AsyncResponse.Failed(IOException(errorMessage, e))
+    }
+}
--- app/src/main/java/com/thomasmacquart/apps/venueapp/venues/domain/VenuesRepo.kt
+++ app/src/main/java/com/thomasmacquart/apps/venueapp/venues/domain/VenuesRepo.kt
@@ -1,13 +1,14 @@
 package com.thomasmacquart.apps.venueapp.venues.domain
 
 import com.thomasmacquart.apps.venueapp.core.AsyncResponse
-import com.thomasmacquart.apps.venueapp.core.BaseRepository
 import com.thomasmacquart.apps.venueapp.core.extensions.exhaustive
+import com.thomasmacquart.apps.venueapp.core.utils.safeApiCall
 import com.thomasmacquart.apps.venueapp.venues.data.api.VenueDetailsApi
 import com.thomasmacquart.apps.venueapp.venues.data.api.VenuesSearchApi
 import com.thomasmacquart.apps.venueapp.venues.data.entities.MapBounds
 import com.thomasmacquart.apps.venueapp.venues.data.entities.Venue
 import com.thomasmacquart.apps.venueapp.venues.data.VenuesCache
+import com.thomasmacquart.apps.venueapp.venues.data.api.SearchResponse
 import com.thomasmacquart.apps.venueapp.venues.data.entities.LatitudeLongitude
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -16,13 +17,17 @@ import javax.inject.Inject
 
 class VenuesRepo @Inject constructor(
     private val api: VenuesSearchApi,
-    private val cache : VenuesCache,
-    private val detailsApi : VenueDetailsApi
-) : BaseRepository() {
+    private val cache: VenuesCache,
+    private val detailsApi: VenueDetailsApi
+) {
 
     private val FOOD_CATEGORY = "4d4b7105d754a06374d81259"
     private val INTENT = "browse"
 
+    /**
+     * We want to display venues quickly.
+     * First we emit the data from cache, then we retrieve venues from network update cache and emit
+     */
     fun loadVenues(target: LatitudeLongitude, bounds: MapBounds): Flow<AsyncResponse<List<Venue>>> =
         flow {
             emit(AsyncResponse.Success(cache.getVenues(bounds)))
@@ -31,24 +36,28 @@ class VenuesRepo @Inject constructor(
             val swBounds = bounds.southWest.formatForQuery()
             val neBounds = bounds.northEast.formatForQuery()
 
-            val result = safeApiResult { api.searchVenues(ll, swBounds, neBounds, FOOD_CATEGORY, INTENT) }
+            val result = safeApiCall(
+                "loadVenues failed"
+            ) { api.searchVenues(ll, swBounds, neBounds, FOOD_CATEGORY, INTENT) }
+
             when (result) {
                 is AsyncResponse.Success -> {
                     cache.updateCache(result.data.venues)
                     emit(AsyncResponse.Success(result.data.venues))
                 }
-                is AsyncResponse.Failed -> emit(AsyncResponse.Failed(IOException("Api call failed")))
+                is AsyncResponse.Failed -> emit(AsyncResponse.Failed(result.exception))
             }.exhaustive
 
 
         }
 
-    suspend fun loadVenueDetails(venueId : String) : AsyncResponse<Venue> {
+    suspend fun loadVenueDetails(venueId: String): AsyncResponse<Venue> {
 
-        val result =  safeApiResult {detailsApi.getVenueDetails(venueId)}
+        val result =
+            safeApiCall("Load venue details failed") { detailsApi.getVenueDetails(venueId) }
         return when (result) {
             is AsyncResponse.Success -> AsyncResponse.Success(result.data.venue)
-            is AsyncResponse.Failed -> AsyncResponse.Failed(IOException("Api call failed"))
+            is AsyncResponse.Failed -> AsyncResponse.Failed(result.exception)
         }
     }
 }
